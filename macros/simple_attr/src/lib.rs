3875827ac3becd9d97496cedf7d7b407737100cf
mahmoud-eltahawy <gemmy9300@gmail.com>
2024-08-29 13:01:02 +0300

    parse simple attributes

--- macros/simple_attr/src/lib.rs
+++ macros/simple_attr/src/lib.rs
@@ -1,21 +1,31 @@
 extern crate proc_macro;
+use core::panic;
+
 use proc_macro::TokenStream;
 
+#[derive(Debug, Clone)]
 enum Props {
     List(Vec<String>),
     Reference(String),
 }
 
+#[derive(Debug, Clone)]
 struct SimpleAttr {
     name: String,
     props: Props,
 }
 
+#[derive(Debug)]
+struct SimpleAttrCooked {
+    name: String,
+    props: Vec<String>,
+}
+
 impl SimpleAttr {
     fn parse(item: TokenStream) -> Vec<Self> {
-        let items = item.to_string();
-        items
+        item.to_string()
             .split(';')
+            .map(|x| x.trim().to_string())
             .flat_map(|x| {
                 if x.is_empty() {
                     return None;
@@ -23,35 +33,61 @@ impl SimpleAttr {
                 let attr = if x.contains('=') {
                     let (header, props) = x.split_once('=').unwrap();
                     SimpleAttr {
-                        name: header.to_string(),
-                        props: Props::Reference(props.to_string()),
+                        name: header.trim().to_string(),
+                        props: Props::Reference(props.trim().to_string()),
                     }
                 } else if x.contains(':') {
                     let (header, props) = x.split_once(':').unwrap();
                     SimpleAttr {
-                        name: header.to_string(),
+                        name: header.trim().to_string(),
                         props: Props::List(
-                            props.split('|').map(|x| x.to_string()).collect::<Vec<_>>(),
+                            props
+                                .split('|')
+                                .map(|x| x.trim().to_string())
+                                .collect::<Vec<_>>(),
                         ),
                     }
                 } else {
-                    return None;
+                    panic!("neither : or = are found");
                 };
                 Some(attr)
             })
             .collect()
+    }
+
+    fn cook(self, ref_list: &Vec<SimpleAttr>) -> SimpleAttrCooked {
+        let Self { name, props } = self;
+
+        let props = match props {
+            Props::List(list) => list,
+            Props::Reference(reference) => {
+                let Props::List(list) = ref_list
+                    .iter()
+                    .find(|x| x.name == reference)
+                    .unwrap()
+                    .props
+                    .clone()
+                else {
+                    panic!("can not reference target");
+                };
+                list
+            }
+        };
+
+        SimpleAttrCooked { name, props }
     }
 }
 
 #[proc_macro]
 pub fn simple_attr(item: TokenStream) -> TokenStream {
     let attrs = SimpleAttr::parse(item);
+    let _attrs = attrs
+        .clone()
+        .into_iter()
+        .map(|x| x.cook(&attrs))
+        .collect::<Vec<_>>();
 
-    r#"
-     fn hello_macro() {
-         println!("hello macro"); 
-     }
-     "#
-    .parse()
-    .unwrap()
+    let tokens_acc = proc_macro2::TokenStream::new();
+
+    TokenStream::from(tokens_acc)
 }
