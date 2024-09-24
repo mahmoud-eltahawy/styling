7620178b3cc57d1f5a6ae5b1fa8b7dffe19ac03e
mahmoud-eltahawy <gemmy9300@gmail.com>
2024-09-24 06:19:30 +0300

    simple attrs tests

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,19 +1,64 @@
-use styling::Style;
-
 fn main() {
-    let result = Style::default()
-        .background_origin()
-        .padding_box()
-        .all()
-        .initial()
-        .accent_color()
-        .dark_red()
-        .accent_color()
-        .red()
-        .margin()
-        .px(20)
-        .bottom()
-        .cm(30)
-        .to_string();
-    println!("{result}");
+    //
+}
+
+#[cfg(test)]
+mod tests {
+    use styling::Style;
+
+    #[test]
+    fn simple_attributes() {
+        let result = Style::default()
+            .all()
+            .initial()
+            .align_content()
+            .center()
+            .align_items()
+            .stretch()
+            .to_string();
+        assert_eq!(
+            "all:initial;align-content:center;align-items:stretch;",
+            &result
+        );
+
+        let result = Style::default()
+            .background_origin()
+            .padding_box()
+            .background_clip()
+            .border_box()
+            .to_string();
+        assert_eq!(
+            "background-origin:padding-box;background-clip:border-box;",
+            &result
+        );
+        let result = Style::default()
+            .border_left_style()
+            .dotted()
+            .border_right_style()
+            .dashed()
+            .to_string();
+        assert_eq!(
+            "border-left-style:dotted;border-right-style:dashed;",
+            &result
+        );
+        let result = Style::default()
+            .border_top_style()
+            .solid()
+            .border_bottom_style()
+            .ridge()
+            .border_collapse()
+            .separate()
+            .to_string();
+        assert_eq!(
+            "border-top-style:solid;border-bottom-style:ridge;border-collapse:separate;",
+            &result
+        );
+        let result = Style::default()
+            .break_after()
+            .always()
+            .break_before()
+            .avoid()
+            .to_string();
+        assert_eq!("break-after:always;break-before:avoid;", &result);
+    }
 }
