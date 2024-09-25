1755a76af558f6e601e31156c7ee649df11b7bc0
mahmoud-eltahawy <gemmy9300@gmail.com>
2024-09-25 21:07:18 +0300

    experimenting

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,74 +1,80 @@
-use styling::experiment::styling;
+use styling::{styling, Home, Styling};
+
+const I: Styling<Home> = styling()
+    .accent_color()
+    .hex(3.5)
+    .accent_color()
+    .hex(5.33)
+    .accent_color()
+    .hex(7.77);
 
 fn main() {
-    let i = styling().accent_color().hex(3).accent_color().hex(5);
-
-    println!("{:#?}", i);
+    println!("{:#?}", I);
     //
 }
 
-#[cfg(test)]
-mod tests {
-    use styling::Style;
+// #[cfg(test)]
+// mod tests {
+//     use styling::Style;
 
-    #[test]
-    fn simple_attributes() {
-        let result = Style::default()
-            .all()
-            .initial()
-            .align_content()
-            .center()
-            .align_items()
-            .stretch()
-            .to_string();
-        assert_eq!(
-            "all:initial;align-content:center;align-items:stretch;",
-            &result
-        );
+//     #[test]
+//     fn simple_attributes() {
+//         let result = Style::default()
+//             .all()
+//             .initial()
+//             .align_content()
+//             .center()
+//             .align_items()
+//             .stretch()
+//             .to_string();
+//         assert_eq!(
+//             "all:initial;align-content:center;align-items:stretch;",
+//             &result
+//         );
 
-        let result = Style::default()
-            .background_origin()
-            .padding_box()
-            .background_clip()
-            .border_box()
-            .to_string();
-        assert_eq!(
-            "background-origin:padding-box;background-clip:border-box;",
-            &result
-        );
-        let result = Style::default()
-            .border_left_style()
-            .dotted()
-            .border_right_style()
-            .dashed()
-            .to_string();
-        assert_eq!(
-            "border-left-style:dotted;border-right-style:dashed;",
-            &result
-        );
-        let result = Style::default()
-            .border_top_style()
-            .solid()
-            .border_bottom_style()
-            .ridge()
-            .border_collapse()
-            .separate()
-            .to_string();
-        assert_eq!(
-            "border-top-style:solid;border-bottom-style:ridge;border-collapse:separate;",
-            &result
-        );
-        let result = Style::default()
-            .break_after()
-            .always()
-            .break_before()
-            .avoid()
-            .cursor()
-            .all_scroll()
-            .to_string();
-        assert_eq!(
-            "break-after:always;break-before:avoid;cursor:all-scroll;",
-            &result
-        );
-    }
-}
+//         let result = Style::default()
+//             .background_origin()
+//             .padding_box()
+//             .background_clip()
+//             .border_box()
+//             .to_string();
+//         assert_eq!(
+//             "background-origin:padding-box;background-clip:border-box;",
+//             &result
+//         );
+//         let result = Style::default()
+//             .border_left_style()
+//             .dotted()
+//             .border_right_style()
+//             .dashed()
+//             .to_string();
+//         assert_eq!(
+//             "border-left-style:dotted;border-right-style:dashed;",
+//             &result
+//         );
+//         let result = Style::default()
+//             .border_top_style()
+//             .solid()
+//             .border_bottom_style()
+//             .ridge()
+//             .border_collapse()
+//             .separate()
+//             .to_string();
+//         assert_eq!(
+//             "border-top-style:solid;border-bottom-style:ridge;border-collapse:separate;",
+//             &result
+//         );
+//         let result = Style::default()
+//             .break_after()
+//             .always()
+//             .break_before()
+//             .avoid()
+//             .cursor()
+//             .all_scroll()
+//             .to_string();
+//         assert_eq!(
+//             "break-after:always;break-before:avoid;cursor:all-scroll;",
+//             &result
+//         );
+//     }
+// }
--- styling/src/lib.rs
+++ styling/src/lib.rs
@@ -1,114 +1,153 @@
-mod attribute;
-mod background;
-mod color;
-mod length;
-mod simple_props;
-pub mod svg;
+// mod attribute;
+// mod background;
+// mod color;
+// mod length;
+// mod simple_props;
+// pub mod svg;
 
-pub type AttributeGetter<T> = Box<dyn FnOnce(T) -> attribute::Attribute>;
-pub type Attributes = Vec<attribute::Attribute>;
+// pub type AttributeGetter<T> = Box<dyn FnOnce(T) -> attribute::Attribute>;
+// pub type Attributes = Vec<attribute::Attribute>;
 
-pub trait StyleState {}
-pub trait PreBaseState<T, R: StyleState + Default>: Sized {
-    fn destruct(self) -> (Attributes, AttributeGetter<T>);
-    fn base(self, position: T) -> Style<R> {
-        let (mut core, fun) = self.destruct();
-        let attr = fun(position);
-        core.push(attr);
-        Style(core, R::default())
-    }
-}
+// pub trait StyleState {}
+// pub trait PreBaseState<T, R: StyleState + Default>: Sized {
+//     fn destruct(self) -> (Attributes, AttributeGetter<T>);
+//     fn base(self, position: T) -> Style<R> {
+//         let (mut core, fun) = self.destruct();
+//         let attr = fun(position);
+//         core.push(attr);
+//         Style(core, R::default())
+//     }
+// }
 
-#[derive(Default)]
-pub struct StyleBaseState<T>(T);
-pub struct Style<T: StyleState>(Attributes, T);
+// #[derive(Default)]
+// pub struct StyleBaseState<T>(T);
+// pub struct Style<T: StyleState>(Attributes, T);
 
-impl StyleState for StyleBaseState<()> {}
-impl<T> StyleState for StyleBaseState<AttributeGetter<T>> {}
-impl<T> PreBaseState<T, StyleBaseState<()>> for Style<StyleBaseState<AttributeGetter<T>>> {
-    fn destruct(self) -> (Attributes, AttributeGetter<T>) {
-        let Self(attrs, StyleBaseState(fun)) = self;
-        (attrs, fun)
-    }
-}
+// impl StyleState for StyleBaseState<()> {}
+// impl<T> StyleState for StyleBaseState<AttributeGetter<T>> {}
+// impl<T> PreBaseState<T, StyleBaseState<()>> for Style<StyleBaseState<AttributeGetter<T>>> {
+//     fn destruct(self) -> (Attributes, AttributeGetter<T>) {
+//         let Self(attrs, StyleBaseState(fun)) = self;
+//         (attrs, fun)
+//     }
+// }
 
-impl<T: StyleState> Style<T> {
-    fn get_attributes(self) -> Attributes {
-        let Self(attrs, _) = self;
-        attrs
-    }
-}
+// impl<T: StyleState> Style<T> {
+//     fn get_attributes(self) -> Attributes {
+//         let Self(attrs, _) = self;
+//         attrs
+//     }
+// }
 
-impl Default for Style<StyleBaseState<()>> {
-    fn default() -> Self {
-        Self(Vec::new(), Default::default())
-    }
-}
+// impl Default for Style<StyleBaseState<()>> {
+//     fn default() -> Self {
+//         Self(Vec::new(), Default::default())
+//     }
+// }
 
 //-----------------------------------------
 
-pub mod experiment {
-    use crate::{color::Color, length::Length};
+use std::marker::PhantomData;
 
-    #[derive(Debug, Clone, Copy, Hash, Eq, PartialEq)]
-    pub enum Attribute {
-        AccentColor(Color),
-        FontSize(Length),
-    }
+#[derive(Debug, Clone, Copy)]
+pub enum Color {
+    Hex(f32),
+    THex(f32),
+    Rgb(f32, f32, f32),
+    Rgba(f32, f32, f32, f32),
+    Hsl(u16, f32, f32),
+    Hsla(u16, f32, f32, f32),
+}
 
-    type Buffer = [Option<Attribute>; 100];
+#[derive(Debug, Clone, Copy)]
+pub enum Length {
+    //absolute
+    Cm(f32),
+    Mm(f32),
+    In(f32),
+    Px(f32),
+    Abs(f32),
+    Pt(f32),
+    Pc(f32),
+    //relative
+    Em(f32),
+    Ex(f32),
+    Ch(f32),
+    Rem(f32),
+    Vw(f32),
+    Vh(f32),
+    Vmin(f32),
+    Vmax(f32),
+    Percent(f32),
+}
 
-    #[derive(Debug)]
-    pub struct Styling(Buffer);
+#[derive(Debug, Clone, Copy)]
+pub enum Attribute {
+    AccentColor(Color),
+    FontSize(Length),
+}
 
-    pub const fn styling() -> Styling {
-        Styling([None; 100])
-    }
+#[derive(Debug)]
+pub struct Styling<T>([Option<Attribute>; 100], PhantomData<T>);
+pub struct Home;
 
-    pub struct AccentColorState(Buffer);
+pub const fn styling() -> Styling<Home> {
+    Styling([None; 100], PhantomData {})
+}
 
-    impl AccentColorState {
-        const fn does_exist(buffer: &Buffer, index: usize) -> Option<usize> {
-            if index < buffer.len() {
-                if matches!(buffer[index], Some(Attribute::AccentColor(_))) {
-                    return Some(index);
-                } else {
-                    return Self::does_exist(buffer, index + 1);
-                }
-            }
-            None
-        }
-    }
-    const fn first_none(buffer: &Buffer, index: usize) -> Option<usize> {
+impl<T> Styling<T> {
+    const fn does_exist(&self, index: usize) -> Option<usize> {
+        let buffer = self.0;
         if index < buffer.len() {
-            if buffer[index].is_none() {
+            if matches!(buffer[index], Some(Attribute::AccentColor(_))) {
                 return Some(index);
             } else {
-                return first_none(buffer, index + 1);
+                return self.does_exist(index + 1);
             }
         }
         None
     }
 
-    impl Styling {
-        pub const fn accent_color(self) -> AccentColorState {
-            let Self(inner) = self;
-            AccentColorState(inner)
+    const fn first_none(&self, index: usize) -> Option<usize> {
+        let buffer = self.0;
+        if index < buffer.len() {
+            if buffer[index].is_none() {
+                return Some(index);
+            } else {
+                return self.first_none(index + 1);
+            }
         }
+        None
     }
 
-    impl AccentColorState {
-        pub const fn hex(self, hex: u32) -> Styling {
-            let Self(mut inner) = self;
-            let index = match Self::does_exist(&inner, 0) {
-                Some(index) => index,
-                None => match first_none(&inner, 0) {
+    const fn target_index(&self) -> usize {
+        match self.does_exist(0) {
+            Some(index) => index,
+            None => {
+                //
+                match self.first_none(0) {
                     Some(index) => index,
                     None => panic!("we ran out of capacity"),
-                },
-            };
-            inner[index] = Some(Attribute::AccentColor(Color::Hex(hex)));
-            Styling(inner)
+                }
+            }
         }
+    }
+}
+
+pub struct AccentColor;
+
+impl Styling<Home> {
+    pub const fn accent_color(self) -> Styling<AccentColor> {
+        let Self(inner, _) = self;
+        Styling(inner, PhantomData {})
+    }
+}
+
+impl Styling<AccentColor> {
+    pub const fn hex(self, hex: f32) -> Styling<Home> {
+        let index = self.target_index();
+        let Self(mut inner, _) = self;
+        inner[index] = Some(Attribute::AccentColor(Color::Hex(hex)));
+        Styling(inner, PhantomData)
     }
 }
