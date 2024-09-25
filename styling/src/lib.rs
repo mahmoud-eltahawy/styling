1bb75d651fdd228d51866d18c5a8582ea03ededd
mahmoud-eltahawy <gemmy9300@gmail.com>
2024-09-26 00:21:03 +0300

    looks promising

--- styling/src/lib.rs
+++ styling/src/lib.rs
@@ -5,107 +5,32 @@
 // mod simple_props;
 // pub mod svg;
 
-// pub type AttributeGetter<T> = Box<dyn FnOnce(T) -> attribute::Attribute>;
-// pub type Attributes = Vec<attribute::Attribute>;
-
-// pub trait StyleState {}
-// pub trait PreBaseState<T, R: StyleState + Default>: Sized {
-//     fn destruct(self) -> (Attributes, AttributeGetter<T>);
-//     fn base(self, position: T) -> Style<R> {
-//         let (mut core, fun) = self.destruct();
-//         let attr = fun(position);
-//         core.push(attr);
-//         Style(core, R::default())
-//     }
-// }
-
-// #[derive(Default)]
-// pub struct StyleBaseState<T>(T);
-// pub struct Style<T: StyleState>(Attributes, T);
-
-// impl StyleState for StyleBaseState<()> {}
-// impl<T> StyleState for StyleBaseState<AttributeGetter<T>> {}
-// impl<T> PreBaseState<T, StyleBaseState<()>> for Style<StyleBaseState<AttributeGetter<T>>> {
-//     fn destruct(self) -> (Attributes, AttributeGetter<T>) {
-//         let Self(attrs, StyleBaseState(fun)) = self;
-//         (attrs, fun)
-//     }
-// }
-
-// impl<T: StyleState> Style<T> {
-//     fn get_attributes(self) -> Attributes {
-//         let Self(attrs, _) = self;
-//         attrs
-//     }
-// }
-
-// impl Default for Style<StyleBaseState<()>> {
-//     fn default() -> Self {
-//         Self(Vec::new(), Default::default())
-//     }
-// }
-
-//-----------------------------------------
-
-use std::{fmt::Display, marker::PhantomData};
-
-#[derive(Debug, Clone, Copy)]
-pub enum Color {
-    Hex(u32),
-    THex(u32),
-    Rgb(f32, f32, f32),
-    Rgba(f32, f32, f32, f32),
-    Hsl(u16, f32, f32),
-    Hsla(u16, f32, f32, f32),
-}
-
-#[derive(Debug, Clone, Copy)]
-pub enum Length {
-    //absolute
-    Cm(f32),
-    Mm(f32),
-    In(f32),
-    Px(f32),
-    Abs(f32),
-    Pt(f32),
-    Pc(f32),
-    //relative
-    Em(f32),
-    Ex(f32),
-    Ch(f32),
-    Rem(f32),
-    Vw(f32),
-    Vh(f32),
-    Vmin(f32),
-    Vmax(f32),
-    Percent(f32),
-}
-#[repr(u16)]
-#[derive(Debug, Clone, Copy)]
-pub enum Attribute {
-    AccentColor(Color),
-    FontSize(Length),
-}
-
-impl Attribute {
-    const fn eq(&self, other: &Self) -> bool {
-        matches!(
-            (self, other),
-            (Attribute::AccentColor(_), Attribute::AccentColor(_))
-                | (Attribute::FontSize(_), Attribute::FontSize(_))
-        )
-    }
-}
+//----------stage definition---------------
+//-------Styling type-----
 
 #[derive(Debug)]
 pub struct Styling<T>([Option<Attribute>; 100], PhantomData<T>);
-pub struct Home;
 
 pub const fn styling() -> Styling<Home> {
     Styling([None; 100], PhantomData)
 }
+//------------------------
+pub struct Home;
+pub struct AccentColor;
+pub struct FontSize;
 
 impl<T> Styling<T> {
+    const fn transform<S>(self) -> Styling<S> {
+        let Self(inner, _) = self;
+        Styling(inner, PhantomData)
+    }
+    const fn add_attr(self, attr: Attribute) -> Styling<Home> {
+        let index = self.target_index(&attr);
+        let Self(mut inner, _) = self;
+        inner[index] = Some(attr);
+        Styling(inner, PhantomData)
+    }
+
     const fn does_exist(&self, other: &Attribute, index: usize) -> Option<usize> {
         let buffer = self.0;
         if index < buffer.len() {
@@ -157,6 +82,82 @@ impl<T> Styling<T> {
         self.size_inner(0)
     }
 }
+//---------------stage functions ---------------
+
+impl Styling<Home> {
+    pub const fn accent_color(self) -> Styling<AccentColor> {
+        self.transform()
+    }
+    pub const fn font_size(self) -> Styling<FontSize> {
+        self.transform()
+    }
+}
+
+impl Styling<AccentColor> {
+    pub const fn hex(self, hex: u32) -> Styling<Home> {
+        self.add_attr(Attribute::AccentColor(Color::Hex(hex)))
+    }
+}
+
+impl Styling<FontSize> {
+    pub const fn px(self, len: f32) -> Styling<Home> {
+        self.add_attr(Attribute::FontSize(Length::Px(len)))
+    }
+}
+
+//------------ attibutes -----------------
+
+use std::{fmt::Display, marker::PhantomData};
+
+#[derive(Debug, Clone, Copy)]
+pub enum Color {
+    Hex(u32),
+    THex(u32),
+    Rgb(f32, f32, f32),
+    Rgba(f32, f32, f32, f32),
+    Hsl(u16, f32, f32),
+    Hsla(u16, f32, f32, f32),
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Length {
+    //absolute
+    Cm(f32),
+    Mm(f32),
+    In(f32),
+    Px(f32),
+    Abs(f32),
+    Pt(f32),
+    Pc(f32),
+    //relative
+    Em(f32),
+    Ex(f32),
+    Ch(f32),
+    Rem(f32),
+    Vw(f32),
+    Vh(f32),
+    Vmin(f32),
+    Vmax(f32),
+    Percent(f32),
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Attribute {
+    AccentColor(Color),
+    FontSize(Length),
+}
+
+impl Attribute {
+    const fn eq(&self, other: &Self) -> bool {
+        matches!(
+            (self, other),
+            (Attribute::AccentColor(_), Attribute::AccentColor(_))
+                | (Attribute::FontSize(_), Attribute::FontSize(_))
+        )
+    }
+}
+
+//--------- display ----------
 
 impl Display for Styling<Home> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -168,40 +169,6 @@ impl Display for Styling<Home> {
         write!(f, "{}", result)
     }
 }
-
-pub struct AccentColor;
-pub struct FontSize;
-
-impl Styling<Home> {
-    pub const fn accent_color(self) -> Styling<AccentColor> {
-        let Self(inner, _) = self;
-        Styling(inner, PhantomData)
-    }
-    pub const fn font_size(self) -> Styling<FontSize> {
-        let Self(inner, _) = self;
-        Styling(inner, PhantomData)
-    }
-}
-
-impl Styling<AccentColor> {
-    pub const fn hex(self, hex: u32) -> Styling<Home> {
-        let index = self.target_index(&Attribute::AccentColor(Color::Hex(0)));
-        let Self(mut inner, _) = self;
-        inner[index] = Some(Attribute::AccentColor(Color::Hex(hex)));
-        Styling(inner, PhantomData)
-    }
-}
-
-impl Styling<FontSize> {
-    pub const fn px(self, len: f32) -> Styling<Home> {
-        let index = self.target_index(&Attribute::FontSize(Length::Cm(0.)));
-        let Self(mut inner, _) = self;
-        inner[index] = Some(Attribute::FontSize(Length::Px(len)));
-        Styling(inner, PhantomData)
-    }
-}
-
-//--------- display ----------
 
 impl Display for Attribute {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
