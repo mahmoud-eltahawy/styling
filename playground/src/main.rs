7e13646b5ef6a59a2ea24b72f9818a8611127936
mahmoud-eltahawy <gemmy9300@gmail.com>
2024-09-29 01:41:28 +0300

    restore tests

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,6 +1,12 @@
 use styling::styling;
 fn main() {
     let styling1 = styling()
+        .align_content()
+        .stretch()
+        .top()
+        .abs(1.)
+        .left()
+        .px(2.)
         .accent_color()
         .hex(0xff0000)
         .accent_color()
@@ -24,68 +30,68 @@ fn main() {
     //
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use styling::Style;
+#[cfg(test)]
+mod tests {
+    use styling::styling;
 
-//     #[test]
-//     fn simple_attributes() {
-//         let result = Style::default()
-//             .all()
-//             .initial()
-//             .align_content()
-//             .center()
-//             .align_items()
-//             .stretch()
-//             .to_string();
-//         assert_eq!(
-//             "all:initial;align-content:center;align-items:stretch;",
-//             &result
-//         );
+    #[test]
+    fn simple_attributes() {
+        let result = styling()
+            .all()
+            .initial()
+            .align_content()
+            .center()
+            .align_items()
+            .stretch()
+            .to_string();
+        assert_eq!(
+            "all:initial;align-content:center;align-items:stretch;",
+            &result
+        );
 
-//         let result = Style::default()
-//             .background_origin()
-//             .padding_box()
-//             .background_clip()
-//             .border_box()
-//             .to_string();
-//         assert_eq!(
-//             "background-origin:padding-box;background-clip:border-box;",
-//             &result
-//         );
-//         let result = Style::default()
-//             .border_left_style()
-//             .dotted()
-//             .border_right_style()
-//             .dashed()
-//             .to_string();
-//         assert_eq!(
-//             "border-left-style:dotted;border-right-style:dashed;",
-//             &result
-//         );
-//         let result = Style::default()
-//             .border_top_style()
-//             .solid()
-//             .border_bottom_style()
-//             .ridge()
-//             .border_collapse()
-//             .separate()
-//             .to_string();
-//         assert_eq!(
-//             "border-top-style:solid;border-bottom-style:ridge;border-collapse:separate;",
-//             &result
-//         );
-//         let result = Style::default()
-//             .break_after()
-//             .always()
-//             .break_before()
-//             .avoid()
-//             .cursor()
-//             .all_scroll()
-//             .to_string();
-//         assert_eq!(
-//             "break-after:always;break-before:avoid;cursor:all-scroll;",
-//             &result
-//         );
-//     }
-// }
+        let result = styling()
+            .background_origin()
+            .padding_box()
+            .background_clip()
+            .border_box()
+            .to_string();
+        assert_eq!(
+            "background-origin:padding-box;background-clip:border-box;",
+            &result
+        );
+        let result = styling()
+            .border_left_style()
+            .dotted()
+            .border_right_style()
+            .dashed()
+            .to_string();
+        assert_eq!(
+            "border-left-style:dotted;border-right-style:dashed;",
+            &result
+        );
+        let result = styling()
+            .border_top_style()
+            .solid()
+            .border_bottom_style()
+            .ridge()
+            .border_collapse()
+            .separate()
+            .to_string();
+        assert_eq!(
+            "border-top-style:solid;border-bottom-style:ridge;border-collapse:separate;",
+            &result
+        );
+        let result = styling()
+            .break_after()
+            .always()
+            .break_before()
+            .avoid()
+            .cursor()
+            .all_scroll()
+            .to_string();
+        assert_eq!(
+            "break-after:always;break-before:avoid;cursor:all-scroll;",
+            &result
+        );
+    }
+}
